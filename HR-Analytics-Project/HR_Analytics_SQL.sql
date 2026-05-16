e7e7c0abb8f5b0d27c007205fc488cf360b4d5b5
Muhamad Adil <mhdadhil08@gmail.com>
2026-05-16 15:53:13 +0530

    Delete HR-Analytics-Project/HR_Analytics_SQL.sql

--- HR-Analytics-Project/HR_Analytics_SQL.sql
+++
@@ -1,6 +0,0 @@
-use hr_analytics;
-select * from hr_cleaned_analysis;
-select department,count(*) as Total_Employees from hr_cleaned_analysis group by department;
-select department,avg(MonthlyIncome) as Avg_Salary from hr_cleaned_analysis group by department;
-select Attrition,count(*) as Total_Employees from hr_cleaned_analysis group by Attrition;
-select OverTime,count(*) as Total_Employess from hr_cleaned_analysis group by OverTime;
